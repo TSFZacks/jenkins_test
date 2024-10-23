8c100f9b04aec3e4e557c176c512520c1703a0c1
TSFZacks <isaackv.s.amaral@gmail.com>
2024-10-23 11:00:18 -0300

    test commit

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -6,30 +5,30 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 def open_chrome_driver():
-
     try:
-
         chrome_options = Options()
-
-        driver = webdriver.Chrome(options=chrome_options)
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')  # Rodar sem interface gráfica
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        
+        # Caminho correto do ChromeDriver baixado pelo Jenkins
+        driver = webdriver.Chrome(executable_path='/opt/chromedriver/chromedriver', options=chrome_options)
 
         return driver
     
-    except Exception:
-
+    except Exception as e:
+        print(f"Erro ao abrir o Chrome: {e}")
         return None
 
 def main(driver):
-
     driver.get('https://www.youtube.com')
     time.sleep(10)
 
     pesquisa_input = driver.find_element(By.XPATH, '//a[@title="Shorts"]')
     pesquisa_input.click()
     time.sleep(10)
-    """like = driver.find_elements(By.XPATH, "//span[contains(text(), 'mil')]")
-    like[0].click()
-    time.sleep(1000)
-    """
+
 driver = open_chrome_driver()
-main(driver)
+if driver:
+    main(driver)
+    driver.quit()
